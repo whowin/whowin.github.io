ebd9841c5a8c660d7c09a34d144f2df5c11a4d27
whowin <hengch@163.com>
2023-09-14 16:25:01 +0800

    new post:part 7 of IPC

---
+++ sourcecodes/100017/shm-ctl.c
@@ -0,0 +1,108 @@
+/*
+ * File: shm-ctl.c
+ * Author: Songqing Hua
+ * email: hengch@163.com
+ * 
+ * (C) 2023 Songqing Hua.
+ * http://whowin.cn/
+ *
+ * Demonstrate how to use shmctl().
+ * Compiled with gcc version 9.4.0 (Ubuntu 9.4.0-1ubuntu1~20.04.1).
+ * Tested on Linux 5.4.0-139-generic #156-Ubuntu SMP Fri Jan 20 17:27:18 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux.
+ *
+ * To compile: $ gcc -Wall shm-ctl.c -o shm-ctl
+ * Usage: $ ./shm-ctl
+ *
+ * Example source code for article 《IPC之七：使用 System V 共享内存段进行进程间通信的实例》
+ * https://whowin.gitee.io/post/blog/linux/0017-systemv-shared-memory/
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h> 
+
+#define BUF_SIZE            256
+
+// Get the attributes of shared memory
+int get_attr(int shmid, struct shmid_ds *shm_ds) {
+    struct shmid_ds shm_attr;
+
+    if (shmctl(shmid, IPC_STAT, &shm_attr) == -1) {
+        perror("shmctl()-IPC_STAT");
+        return -1;
+    }
+
+    *shm_ds = shm_attr;
+    return 0;
+}
+
+int main() {
+    key_t key;
+    int shmid;
+
+    if ((key = ftok("./", 'Z')) == -1) {
+        perror("ftok()");
+        exit(EXIT_FAILURE);
+    }
+    // Create/Get the ID of shared memorg segment
+    if ((shmid = shmget(key, BUF_SIZE, IPC_CREAT | 0666)) == -1) {
+        perror("shmget()");
+        exit(EXIT_FAILURE);
+    }
+
+    struct shmid_ds shm_attr;
+
+    // IPC_STAT
+    if (get_attr(shmid, &shm_attr) == -1) {
+        goto PROG_EXIT;
+    }
+    printf("The R/W permission is %04o\n", shm_attr.shm_perm.mode & 0x1FF);
+
+    // IPC_SET
+    shm_attr.shm_perm.mode &= 0x1F8;
+    if (shmctl(shmid, IPC_SET, &shm_attr) == -1) {
+        perror("shmctl()-IPC_SET");
+        goto PROG_EXIT;
+    }
+
+    if (get_attr(shmid, &shm_attr) == -1) {
+        goto PROG_EXIT;
+    }
+    printf("The modified R/W permission is %04o\n", shm_attr.shm_perm.mode & 0x1FF);
+    printf("The LOCKED flag: %s\n", (shm_attr.shm_perm.mode & SHM_LOCKED) ? "True" : "False");
+
+    // SHM_INFO
+    struct shm_info shminfo;
+    if (shmctl(shmid, SHM_INFO, (struct shmid_ds *)&shminfo) == -1) {
+        perror("shmctl()-SHM_INFO");
+        goto PROG_EXIT;
+    }
+    printf("# of currently existing segments: %d\n", shminfo.used_ids);
+    printf("Total number of shared memory pages: %ld\n", shminfo.shm_tot);
+    printf("# of resident shared memory pages: %ld\n", shminfo.shm_rss);
+    printf("# of swapped shared memory pages: %ld\n", shminfo.shm_swp);
+
+    // SHM_LOCK
+    if (shmctl(shmid, SHM_LOCK, NULL) == -1) {
+        perror("shmctl()-SHM_LOCK");
+        goto PROG_EXIT;
+    }
+    if (get_attr(shmid, &shm_attr) == -1) {
+        goto PROG_EXIT;
+    }
+    printf("The LOCKED flag: %s\n", (shm_attr.shm_perm.mode & SHM_LOCKED) ? "True" : "False");
+
+    shmctl(shmid, SHM_UNLOCK, NULL);
+
+PROG_EXIT:
+    shmctl(shmid, IPC_RMID, NULL);
+    exit(EXIT_SUCCESS);
+
+}
---
+++ sourcecodes/100017/shm-public.h
@@ -0,0 +1,268 @@
+/*
+ * File: shm-public.c
+ * Author: Songqing Hua
+ * email: hengch@163.com
+ * 
+ * (C) 2023 Songqing Hua.
+ * http://whowin.cn/
+ *
+ * Demonstrate multiple client communcating with single server using System V shared memory segment.
+ * This is a including file to be included by server and client parts of the demonstration program.
+ * Compiled with gcc version 9.4.0 (Ubuntu 9.4.0-1ubuntu1~20.04.1).
+ * Tested on Linux 5.4.0-139-generic #156-Ubuntu SMP Fri Jan 20 17:27:18 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux.
+ *
+ * To compile: $ N/A
+ * Usage: $ N/A
+ *
+ * Example source code for article 《IPC之七：使用 System V 共享内存段进行进程间通信的实例》
+ * https://whowin.gitee.io/post/blog/linux/0017-systemv-shared-memory/
+ * 
+ */
+
+#ifndef _SHM_PUBLIC_H
+#define _SHM_PUBLIC_H   1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h> 
+#include <sys/sem.h>
+
+#define MAX_STR_LEN             256     // Max. length of each string
+#define MAX_BUFFERS             10      // Max. number of buffer
+
+// the 3 macros below are for generating key_t(s) of semaphore set and shared memory segment
+#define SEMSET_KEY              "/tmp/sems_key"
+#define SHARED_MEMORY_KEY       "/tmp/shared_memory_key"
+#define PROJECT_ID              'K'
+
+#define SEM_NUM                 3       // number of semaphores in the set
+#define SEM_MUTEX               0       // index # of MUTEX semaphore in the set
+#define SEM_BUF_FULL            1       // index # of BUFFER_FULL semphore in the set
+#define SEM_BUF_EMPTY           2       // index # of BUFFER_EMPTY semaphore in the set
+
+#define FLAG_SERVER             0       // indicates the current operation is from server
+#define FLAG_CLIENT             1       // indicates the current operation is from client
+
+#define SEM_PERMS               0660    // R/W permissions for semaphore set
+#define SHM_PERMS               0660    // R/W permissions for shared memory
+
+#define FONT_RED                "\033[31m"
+#define FONT_YELLOW             "\033[33m"
+#define FONT_DEFAULT            "\033[0m"
+
+struct shared_memory {
+    char buf[MAX_BUFFERS][MAX_STR_LEN];     // buffer which can store MAX_BUFFERS number of strings.
+    int string_index;                       // string index using for placing a string into buffer
+    int print_index;                        // print index using for taking out a string from buffer
+};
+
+// For setting semaphore's value
+union semun {
+    int val;
+    struct semid_ds *buf;
+    ushort array[1];
+};
+
+/****************************************************************
+ * Function: void error (char *msg)
+ * Description: print system error message and 
+ *              send out SIGINT signal to itself and parent
+ * 
+ * Argument:
+ *      msg     message printed before system error message
+ * Return:      NONE
+ ****************************************************************/
+void error (char *msg) {
+    perror(msg);
+    kill(getppid(), SIGINT);        // signal parent
+    kill(getpid(), SIGINT);         // signal itself
+}
+/*****************************************************************
+ * Function: void touch_key_files()
+ * Description: Create 2 empty files for generating key_t
+ * 
+ * Arguments: NONE
+ * Return: NONE
+ *****************************************************************/
+void touch_key_files() {
+    int fd = open(SEMSET_KEY, O_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    close(fd);
+
+    fd = open(SHARED_MEMORY_KEY, O_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    close(fd);
+}
+/************************************************************************
+ * Function: int get_semset(int flag)
+ * Description: Create a semaphore set for server
+ *              Open a semaphore set for client
+ * 
+ * Arguments:   flag        FLAG_SERVER: caller is server
+ *                          FLAG_CLIENT: caller is client
+ * Return:  on success, return the ID of semaphore set
+ *          on error, print a error message and exit
+ ************************************************************************/
+int get_semset(int flag) {
+    key_t s_key;
+    int semid;
+    int semflag = IPC_EXCL;
+
+    union semun sem_attr;
+
+    s_key = ftok(SEMSET_KEY, PROJECT_ID);
+    if (s_key == -1) {
+        error("ftok");
+    }
+    if (flag == FLAG_SERVER) {
+        semflag = semflag | IPC_CREAT | SEM_PERMS;
+    }
+    semid = semget(s_key, SEM_NUM, semflag);
+    if (semid == -1) {
+        if (flag == FLAG_SERVER) {
+            printf("%sCan't create a new semaphore set.%s\n", FONT_RED, FONT_DEFAULT);
+        } else {
+            if (errno == ENOENT) {
+                printf("%sThe semaphore set to be obained doesn't exist.%s\n", FONT_RED, FONT_DEFAULT);
+            } else {}
+                printf("%sCan't get the semaphore set with unknown reason.%s\n", FONT_RED, FONT_DEFAULT);
+        }
+        error("semget");
+    }
+    if (flag == FLAG_SERVER) {
+        // initial the semaphore set. 
+        sem_attr.val = MAX_BUFFERS;     // counting semaphore for buffer full
+        if (semctl(semid, SEM_BUF_FULL, SETVAL, sem_attr) == -1) {
+            error("semctl()-SETVAL-SEM_BUF_FULL");
+        }
+        sem_attr.val = 0;               // counting semaphore for buffer empty
+        if (semctl(semid, SEM_BUF_EMPTY, SETVAL, sem_attr) == -1) {
+            error("semctl-SETVAL-SEM_BUF_EMPTY");
+        }
+        sem_attr.val = 1;               // unlocked the MUTEX
+        if (semctl(semid, SEM_MUTEX, SETVAL, sem_attr) == -1) {
+            error("semctl-SETVAL-SEM_MUTEX");
+        }
+    }
+
+    return semid;
+}
+/*********************************************************************************
+ * Function: int get_shared_mem(int flag, struct shared_memory **shared_mem_p)
+ * Description: Create a shared memory segment for server
+ *              Open a shared memory segment for client
+ * 
+ * Arguments:   flag            FLAG_SERVER: caller is server
+ *                              FLAG_CLIENT: caller is client
+ *              shared_mem_p    point to address pointer to attach the shared
+ *                              memory segment
+ * Return:  on success, return the ID of the shared memory segmant
+ *          on error, print a error message and exit
+ *********************************************************************************/
+int get_shared_mem(int flag, struct shared_memory **shared_mem_p) {
+    key_t s_key;
+    int shmid;
+    int shmflag = IPC_EXCL;
+    struct shared_memory *shared_mem_ptr;
+
+    s_key = ftok(SHARED_MEMORY_KEY, PROJECT_ID);
+    if (s_key == -1) {
+        error("ftok");
+    }
+    if (flag == FLAG_SERVER) {
+        shmflag = shmflag | IPC_CREAT | SHM_PERMS;
+    }
+    shmid = shmget(s_key, sizeof(struct shared_memory), shmflag);
+    if (shmid == -1) {
+        if (flag == FLAG_SERVER) {
+            printf("%sCan't create a new shared memory segment.%s\n", FONT_RED, FONT_DEFAULT);
+        } else {
+            if (errno == ENOENT) {
+                printf("%sThe shared memory segment to be obained doesn't exist.%s\n", FONT_RED, FONT_DEFAULT);
+            } else {}
+                printf("%sCan't get the shared memory segment with unknown reason.%s\n", FONT_RED, FONT_DEFAULT);
+        }
+        error("shmget");
+    }
+    shared_mem_ptr = (struct shared_memory *)shmat(shmid, NULL, flag);
+    if (shared_mem_ptr == (struct shared_memory *)-1) {
+        printf("%sCan't attach the shared memory segment to the user address space.%s\n", FONT_RED, FONT_DEFAULT);
+        error("shmat");
+    }
+    *shared_mem_p = shared_mem_ptr;
+    if (flag == FLAG_SERVER) {
+        // Initialize the shared memory
+        shared_mem_ptr->string_index = shared_mem_ptr->print_index = 0;
+    }
+
+    return shmid;
+}
+/*********************************************************************************
+ * Function: void sem_op(int semid, int sem_index, int value, int flag)
+ * Description: Operate a semaphore's value in the set
+ * 
+ * Arguments:
+ *      semid           ID of the semaphore set
+ *      sem_index       semaphore index in the set
+ *      value           adjusted value of the semaphore
+ *      flag            FLAG_SERVER: caller is server
+ *                      FLAG_CLIENT: caller is client
+ 
+ * Return:  NONE
+ *********************************************************************************/
+void sem_op(int semid, int sem_index, int value, int flag) {
+    struct sembuf asem[1];
+
+    asem[0].sem_num = sem_index;
+    asem[0].sem_op = value;
+    asem[0].sem_flg = 0;
+
+    if (semop(semid, asem, 1) == -1) {
+        perror("semop");
+        if (flag == FLAG_CLIENT) {
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+/*********************************************************************************
+ * Function: void sem_p(int semid, int sem_index, int flag)
+ * Description: P operation for a semaphore in the set
+ * 
+ * Arguments:
+ *      semid           ID of the semaphore set
+ *      sem_index       semaphore index in the set
+ *      flag            FLAG_SERVER: caller is server
+ *                      FLAG_CLIENT: caller is client
+ 
+ * Return:  NONE
+ *********************************************************************************/
+void sem_p(int semid, int sem_index, int flag) {
+    sem_op(semid, sem_index, -1, flag);
+}
+/*********************************************************************************
+ * Function: void sem_p(int semid, int sem_index, int flag)
+ * Description: V operation for a semaphore in the set
+ * 
+ * Arguments:
+ *      semid           ID of the semaphore set
+ *      sem_index       semaphore index in the set
+ *      flag            FLAG_SERVER: caller is server
+ *                      FLAG_CLIENT: caller is client
+ 
+ * Return:  NONE
+ *********************************************************************************/
+void sem_v(int semid, int sem_index, int flag) {
+    sem_op(semid, sem_index, 1, flag);
+}
+
+#endif
